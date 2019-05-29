a580e5d9fe432cef677014991a528d7f23195284
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-05-29 10:13:39 +0200

    Ajout de la collectiondes donnees
    Il est desormais possible de faire une, separation des donnees lus depuis le fichier.

--- Lecture_NMEA.py
+++ Lecture_NMEA.py
@@ -18,7 +18,18 @@ def lecturefichier():
             print("Données non conformes : ", line)
     in_file.close()
     #-------------Analyse des données GPS------------
-    Liste_position=[]
-    for line in data:
-        if line[0]=='$GPGGA':
-    
+    Liste_time_pos=[]
+    for line in data:#on parcours toutes les lignes du fichier
+        if line[0]=='$GPGGA':#On regarde la ligne contenant ttes les infos utiles
+            Ligne=[]#On crée une liste contenant les infos temps et position
+            Ligne.append('Temps:')
+            Ligne.append(line[1])
+            Ligne.append('Latitude:')
+            Ligne.append(line[2])
+            Ligne.append('Longitude:')
+            Ligne.append(line[4])
+            Ligne.append('Altitude:')
+            Ligne.append(line[9])
+            Ligne.append('Nb satellites:')
+            Ligne.append(line[7])
+    Liste_time_pos.append(Ligne)
