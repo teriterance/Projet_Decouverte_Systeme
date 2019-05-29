a949b409456d0992aef41bf1a6fd6d73a703ae72
HippolyteCleris <51151582+HippolyteCleris@users.noreply.github.com>
2019-05-29 09:24:31 +0200

    Add files via upload
    Premier fichier,
    permet d'ouvrir le NMEA et de stocker l'information dans une liste de liste

---
+++ Lecture NMEA.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 08:48:34 2019
+
+@author: Cléris
+"""
+
+if __name__ == '__main__':
+    
+    #-------Ouverture et création d'une liste data----
+    #-------------contenant les infos-------------
+    in_file = open('data_uv24.nmea', 'r')
+    data = []
+
+    for line in in_file:
+        data_txt = line.split()
+        try:
+            datal = [str(x) for x in data_txt]
+            data.append(datal)
+        except Exception:
+            print("Données non conformes : ", line)
+
+    in_file.close()
+    
+    #-------------Analyse des données GPS------------
+    
