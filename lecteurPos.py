b8b8bff1f82d040c04d5a1d0c91c310f1118c2d5
HippolyteCleris <51151582+HippolyteCleris@users.noreply.github.com>
2019-05-29 15:09:43 +0200

    Update lecteurPos.py
    J'ai ajouté ici les fonctions permettant les calculs de précisions, de justesse et de fidélité.
    Et dans la fonction collecte_data, celle ci peut retourner des données de longitude, latitude, altitude dans des listes séparées.

--- lecteurPos.py
+++ lecteurPos.py
@@ -1,7 +1,7 @@
 from threading import Timer
 import time
 import serial
-
+import numpy as np
 
 # fonction principale
 def main():
@@ -19,7 +19,7 @@ def lectureSerie(): # fonction de lecture série
         
         
 def collecte_data():
-    data=[]
+    data=[],Latitude=[],Longitude=[],Altitude=[]
     while serialPort.inWaiting():
         line=[]
         line.append(serialPort.readline().decode("ascii").split(','))
@@ -28,15 +28,32 @@ def collecte_data():
             Ligne.append('Temps:')
             Ligne.append(line[1])
             Ligne.append('Latitude:')
+            Latitude.append(line[2])
             Ligne.append(line[2])
             Ligne.append('Longitude:')
+            Longitude.append(line[4])
             Ligne.append(line[4])
             Ligne.append('Altitude:')
+            Altitude.append(line[9])
             Ligne.append(line[9])
             Ligne.append('Nb satellites:')
             Ligne.append(line[7])
             data.append(Ligne)
+    #return Latitude, Longitude, Altitude
     print(data)
+    
+def justesse(Liste_valeurs, Valeur_vraie):
+    m=sum(Liste_valeurs, 0.0) / len(Liste_valeurs)
+    e_justesse=abs(m-Valeur_vraie)
+    return e_justesse
+def fidelite(Liste_valeurs):
+    return np.std(Liste_valeurs)
+def precision(Liste_valeurs, Valeur_vraie):
+    e_justesse=justesse(Liste_valeurs, Valeur_vraie)
+    e_fidelite=fidelite(Liste_valeurs)
+    e_precision=((e_justesse**2)+(e_fidelite)**2)**(-2)
+    return e_precision
+
 #--- obligatoire pour rendre code exécutable ---
 if __name__ == "__main__": # cette condition est vraie si le fichier est le programme exécuté
     main()# appelle la fonction principale
