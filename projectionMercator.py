2d5aabbd3314047274918c4a92f82027fb149350
HippolyteCleris <51151582+HippolyteCleris@users.noreply.github.com>
2019-06-05 11:23:55 +0200

    Update projectionMercator.py

--- projectionMercator.py
+++ projectionMercator.py
@@ -12,22 +12,28 @@ import numpy as np
 #https://fr.wikipedia.org/wiki/Transverse_universelle_de_Mercator 
 
 class Projection_Mercator():
+    
     def __init__(self, latitude, longitude, lambda0):
         self.rayon_terre=6378.137#En km
         self.excentricite=0.0818192
         self.phi=latitude
         self.lambd=longitude
         self.lambd_0=lambda0
-        
-    def nhu():
+        self.k0=0.996
+    
+    def nhu(self):
         return 1/((1-(self.excentricite*np.sin(self.phi))**2)**(-2))
-    def Var_A():
+    
+    def Var_A(self):
         return (self.lambd-self.lambd_0)*np.cos(self.phi)
-    def Var_T():
+    
+    def Var_T(self):
         return (np.tan(self.phi))**2
-    def Var_C():
+    
+    def Var_C(self):
         return ((self.excentricite)**2/(1-self.excentricite)**2)*((np.cos(self.phi))**2)
-    def S():
+    
+    def S(self):
         e=self.excentricite
         terme1=(1-((e)**2/4)-(3*(e)**4/64)-(5*(e)**6/256))*np.sin(self.phi)
         terme2=((3*(e)**2/8)+(3*(e)**4/32)+(45*(e)**6/1024))*np.sin(2*self.phi)
@@ -35,3 +41,29 @@ class Projection_Mercator():
         terme4=((35*(e)**6/3072))*np.sin(6*self.phi)
         solution= terme1-terme2+terme3-terme4
         return solution
+
+    def abscisse(self):
+        a=self.rayon_terre
+        k0=self.k0
+        A=self.Var_A()
+        T=self.Var_T()
+        C=self.Var_C()
+        nhu=self.nhu()
+        terme1=A+(1-T+C)*(A**3)/6
+        terme2=(5-18*T+T**2)*(A**5)/120
+        E=500+k0*a*nhu*(terme1+terme2)
+        return E
+
+    def ordonne(self):
+        a=self.rayon_terre
+        k0=self.k0
+        A=self.Var_A()
+        T=self.Var_T()
+        C=self.Var_C()
+        nhu=self.nhu()
+        S=self.S()
+        phi=self.phi
+        terme1=(A**2/2)+(5-T+9*C+4*C**2)*(A**4/24)
+        terme2=(61-58*T+T**2)*(A**6/720)
+        N=k0*a*(S+nhu*np.tan(phi)*(terme1+terme2))
+        return N
