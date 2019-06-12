9fca51f9bdfbb09f05b9a70a3707a837a4186080
HippolyteCleris <51151582+HippolyteCleris@users.noreply.github.com>
2019-06-12 15:03:23 +0200

    Update codeFonctionel.py

--- codeFonctionel.py
+++ codeFonctionel.py
@@ -5,7 +5,7 @@ import numpy as np
 
 gps =  GPS()
 valVrai_9000 = [139.4655, 48.418407975, 4.474412007]
-valVrai_8000 = [139.460, 48.418361635, 4.4744215119]
+valVrai_8000 = [139.460, 48.418361635, 4.474421519]
 gps.acDonne()
 #pour le point 9000
 print(gps.calculFidelite(), "fidelite")
@@ -13,9 +13,19 @@ print(gps.calculPrecision(valVrai_9000), "Precision")
 print(gps.calculJustesse(valVrai_9000), "justesse")
 projection  = Projection_Mercator(4.474412007)
 i = 0
-altitude = np.zeros([1,10])
-latitude = np.zeros([1,10])
-longitude = np.zeros([1,10])
+altitude = np.zeros((1,10))
+latitude = np.zeros((1,10))
+longitude = np.zeros((1,10))
+liste_abscisse=[]
+liste_ordonnee=[]
+
 while i < 10:
-    [altitude[i], latitude[i], longitude[i]], nb_sat =gps.acDonneUnit()
+    [alti, lati, longi], nb_sat =gps.acDonneUnit()
+    altitude[0,i]=alti
+    latitude[0,i]=lati
+    longi[0,i]=longi
+    x,y=projection.calcul(lati, longi)
+    liste_abscisse.append(x)
+    liste_ordonne.append(y)
+    i+=1
 plt.plot()
