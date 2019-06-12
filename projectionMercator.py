005e4e9d109ef747d1e4bb0fda69ff49b060069b
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-06-12 08:41:34 +0200

    untroduction de la fonction calcul

--- projectionMercator.py
+++ projectionMercator.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jun  5 09:06:43 2019
-
 @author: Cléris
 """
 #
@@ -13,11 +12,11 @@ import numpy as np
 
 class Projection_Mercator():
     
-    def __init__(self, latitude, longitude, lambda0):
+    def __init__(self,latitude, longitude, lambda0):
         self.rayon_terre=6378.137#En km
         self.excentricite=0.0818192
-        self.phi=latitude
-        self.lambd=longitude
+        self.phi= 0
+        self.lambd= 0
         self.lambd_0=lambda0
         self.k0=0.996
     
@@ -67,3 +66,10 @@ class Projection_Mercator():
         terme2=(61-58*T+T**2)*(A**6/720)
         N=k0*a*(S+nhu*np.tan(phi)*(terme1+terme2))
         return N
+    
+    def calcul(self, latitude, longitude):
+        """en plus de calcul des coordonee, il permet de mettre a jour la latitude et la longitude"""
+        self.phi= latitude
+        self.lambd= longitude
+        return self.abscisse(), self.ordonne()
+
