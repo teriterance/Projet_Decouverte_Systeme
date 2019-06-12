92f1ec5362830524d92f233ea02c915cbcc0b0e5
Pierre Gabin FODOP GUMETE <gabinfodop@gmail.com>
2019-06-12 11:33:38 +0200

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -11,10 +11,10 @@ class GPS(object):
         self.precision = [0,0,0]
         self.fidelite = [0,0,0]
         self.e_justesse = [0,0,0]
-        self.dernierevaleur = [0, 0, 0]#les derniere valeur en altitude longitude altitude
+        self.dernierevaleur = [0, 0, 0]#les derniere valeur en altitude latitude longitude
         self.valeursMoyenne = 0
         self.nb_satellite = 0
-        self.Liste_valeurs = [[],[],[]]# en ordre , l'altitude, la longitude et la latitude 
+        self.Liste_valeurs = [[],[],[]]# en ordre , l'altitude, la latitude et la longitude 
 
     def retirerVal(self):
         self.Liste_valeurs[0].pop(0)
@@ -26,16 +26,17 @@ class GPS(object):
         if len(self.Liste_valeurs[0]) > 100: 
             #on limite la taille a 30 valeur
             self.retirerVal()
-        self.dernierevaleur  = [altitude, longitude, latitude]
-        self.Liste_valeurs[0].append(altitude)
-        self.Liste_valeurs[1].append(longitude)
-        self.Liste_valeurs[2].append(latitude)
+        self.dernierevaleur  = [altitude, latitude, longitude]
+        self.Liste_valeurs[0].append(altitude)        
+        self.Liste_valeurs[1].append(latitude)
+        self.Liste_valeurs[2].append(longitude)
+
 
     def lectureSerie(self): 
         # fonction de lecture série
         return self.serialPort.readline().decode("ascii")# (auto)-rappelle la fonction dans 100ms
 
-    def calculJustesse(self, Valeurs_vraies):
+    def calculJustesse(self, Valeur_vraie):
         #il calcule la justesse, il attend la valeur vraie, celle du proflex 
         self.moyene = [sum(self.Liste_valeurs[0] , 0.0) / len(self.Liste_valeurs[0]), sum(self.Liste_valeurs[1] , 0.0) / len(self.Liste_valeurs[1]),sum(self.Liste_valeurs[2] , 0.0) / len(self.Liste_valeurs[2])]
         self.e_justesse = [abs(self.moyene[0]-Valeur_vraie[0]),abs(self.moyene[1]-Valeur_vraie[1]),abs(self.moyene[2]-Valeur_vraie[2])]
@@ -50,17 +51,39 @@ class GPS(object):
         #il calcule la precision
         self.e_justesse= self.calculJustesse(Valeur_vraie)
         self.e_fidelite= self.calculFidelite()
-        self.e_precision=[((self.e_justesse[0]**2)+(self.e_fidelite[0])**2)**(-2), ((self.e_justesse[1]**2)+(self.e_fidelite[1])**2)**(-2), ((self.e_justesse[2]**2)+(self.e_fidelite[2])**2)**(-2)] 
+        self.e_precision=[((self.e_justesse[0]**2)+(self.e_fidelite[0])**2)**(1/2), ((self.e_justesse[1]**2)+(self.e_fidelite[1])**2)**(1/2), ((self.e_justesse[2]**2)+(self.e_fidelite[2])**2)**(1/2)] 
         return self.e_precision
+
+    def convMinutetoDegreLat(self,latitude):
+        sortie = 0
+        sortie = sortie+ int(latitude[0:2])
+        sortie = sortie+ 0.0166667*float(latitude[2:8])
+        return sortie
     
+    def convMinutetoDegreLong(self, longitude):
+        sortie = 0
+        sortie = sortie+ int(longitude[0:3])
+        sortie = sortie+ 0.0166667*float(longitude[3:9])
+        return sortie
+
     def acDonne(self):
-        while self.nombreMesure < 100:
+        self.nombreMesure = 0
+        while self.nombreMesure < 20:
             line = self.lectureSerie().split(',')
             print(self.dernierevaleur, self.nombreMesure, self.nb_satellite)
             if  line[6]!=0 and line[0]=='$GPGGA':#On regarde la ligne contenant ttes les infos utiles
-                self.ajouterVal(float(line[9]), float(line[2]), float(line[4]))
+                line[2]
+                self.ajouterVal(float(line[9]), self.convMinutetoDegreLat(line[2]), self.convMinutetoDegreLong(line[4]))
                 self.nb_satellite = int(line[7])
                 self.nombreMesure = self.nombreMesure + 1
+    
+    def acDonneUnit(self):
+        line = self.lectureSerie().split(',')
+        print(self.dernierevaleur, self.nb_satellite)
+        if  line[6]!=0 and line[0]=='$GPGGA':#On regarde la ligne contenant ttes les infos utiles
+            self.ajouterVal(float(line[9]), self.convMinutetoDegreLat(line[2]), self.convMinutetoDegreLong(line[4]))
+            self.nb_satellite = int(line[7])
+        return self.dernierevaleur, self.nb_satellite
     
     def lectureReel(self):
         line = self.lectureSerie().split(',')
